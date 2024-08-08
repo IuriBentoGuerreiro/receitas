d1663ee79db57f6cff7a376ee17b3804836f49bc
iuri <bentoiuri@gmail.com>
2024-08-08 08:16:17 -0300

    adiciona conexao com banco de dados

---
+++ src/test/java/com/iuri/receitas/ReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuri.receitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
