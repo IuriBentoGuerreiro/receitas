e35a7d214ee425d054f512f9011aa18a96834d86
iuri <bentoiuri@gmail.com>
2024-08-08 08:54:44 -0300

    adiciona ReceitaController

---
+++ src/main/java/com/iuri/receitas/controller/ReceitaController.java
@@ -0,0 +1,48 @@
+package com.iuri.receitas.controller;
+
+import com.iuri.receitas.dto.ReceitaRequest;
+import com.iuri.receitas.dto.ReceitaResponse;
+import com.iuri.receitas.service.ReceitaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/receita")
+public class ReceitaController {
+
+    @Autowired
+    private ReceitaService receitaService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ReceitaResponse salvar(@RequestBody ReceitaRequest request){
+        return receitaService.salvar(request);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ReceitaResponse> listar(){
+        return receitaService.listar();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ReceitaResponse pegarPorId(@PathVariable Integer id){
+        return ReceitaResponse.converter(receitaService.pegarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ReceitaResponse atualizar(@PathVariable Integer id, @RequestBody ReceitaRequest request){
+        return ReceitaResponse.converter(receitaService.atualizar(id, request));
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id){
+        receitaService.deletar(id);
+    }
+}
